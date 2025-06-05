c5dc3547141d2271898dd0e5776a0bda919a7318
FaizGhazali0712 <faiz0712@student.abudzar.sch.id>
2025-06-05 09:01:34 +0700

    Menambahkan file SAS.jsx

---
+++ antum/src/SAS.jsx
@@ -0,0 +1,11 @@
+function SAS() {
+  return (
+    <div>
+      <h1>Halo dari Komponen SAS</h1>
+      <p>Ini paragraf dari SAS.jsx</p>
+      <img src="https://via.placeholder.com/200" alt="Gambar contoh" />
+    </div>
+  );
+}
+
+export default SAS;
